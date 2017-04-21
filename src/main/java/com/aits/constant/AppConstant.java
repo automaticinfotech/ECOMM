11824638b0a9a2f3d356fba87343d8e69d89c158
aits <aits@AITSLU177>
2017-04-21 11:11:04 +0530

    pj

--- src/main/java/com/aits/constant/AppConstant.java
+++ src/main/java/com/aits/constant/AppConstant.java
@@ -23,22 +23,107 @@ public interface AppConstant {
 	String UPDATE_CATEGORY_MASTER = "/updateCategoryMaster";
 	String UPDATE_CATEGORY_STATUS = "/updateCategoryStatus";
 	
-	/*   added by  santosh  12/04/2017 */	
 	
 	
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*   added by  santosh  12/04/2017 */		
 	String AdminStates="/adminStates";
 	String SAVE_STATE="/saveState";
 	String ADMIN_INACTIVE_STATE="/inactiveState";
 	String ADMIN_ACTIVE_STATE="/activeState";
 	String ADMIN_EDIT_STATE="/editState";
 	String SAVE_CITY="/saveCity";
+	String ADMIN_CITYS="adminAllCitys";
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	/*Discount Mgmt*/
 	String GET_DISCOUNT_LIST="/getDiscountMasterList";
 	String ADD_DISCOUNT="/addDiscountMasterInformation";
 	String GET_DISCOUNT_INFO_BY_ID="/getDiscountMasterInformationById/";
+	String GET_DISCOUNT_MAPPER_LIST="/getSubProductDiscountMapperList";
+	String GET_SUB_PRODUCT_LIST="/getSubProductMasterList";
+	String ADD_DISCOUNT_MAPPER_INFO="/insertSubProductDiscountMapperInformation";
 	
 	
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
+
+
